a72ed1f619a5407c588135abaee14cf271befd3c
GarikFanarik <148388533+GarikFanarik@users.noreply.github.com>
2025-01-31 14:59:34 +0000

    update

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.session_state.stock = st.text_input("Enter the desired Stock")
 if st.session_state.stock:
     res = get_financial_ratios(st.session_state.stock)
     st.subheader(res[1])
-    st.dataframe(res[0].T)
-    #st.dataframe(pd.concat([res[0].T, compare_with_target_values(res[0]).T]))
+    #st.dataframe(res[0].T)
+    st.dataframe(pd.concat([res[0].T, compare_with_target_values(res[0]).T]))
 
